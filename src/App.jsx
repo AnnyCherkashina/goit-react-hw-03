0a2e5f862b2297ba715385be3e6820c4b699eff1
AnnyCherkashina <149014624+AnnyCherkashina@users.noreply.github.com>
2024-06-05 15:00:29 +0300

    update hw3

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import ContactForm from './components/ContactForm/ContactForm'
+import SearchBox from './components/SearchBox/SearchBox'
+import ContactList from './components/ContactList/ContactList'
+import contactsBase from './assets/contacts.json'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = JSON.parse(window.localStorage.getItem('contacts'))
+    if (savedContacts?.length) {
+      return savedContacts
+    }
+    return contactsBase
+  })
+  const [searchStr, setSearch] = useState("")
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts))
+  }, [contacts])
+
+  const handleDelete = id => {
+    setContacts(prev => prev.filter(item => item.id !== id))
+  }
+
+  const addContact = (contact) => {
+    setContacts(prev => [...prev, contact])
+  }
+
+  const getFilteredData = () => {
+    return contacts.filter(
+      item =>
+        item.name.toLowerCase().includes(searchStr.toLowerCase())
+    )
+  }
+  const filteredData = getFilteredData();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <SearchBox searchStr={searchStr} setSearch={setSearch} />
+      <ContactList filteredData={filteredData} searchStr={searchStr} handleDelete={handleDelete} />
+    </div>
   )
 }
 
